57ae9658c93c730daec571cc6d29a52218a3f5e7
ashwalk33r <58489098+ashwalk33r@users.noreply.github.com>
2020-09-06 15:34:53 +0200

    Create run-on-likes-page.js

---
+++ run-on-likes-page.js
@@ -0,0 +1,48 @@
+(() => {
+	const selectorsAll = [
+		'.sc-button-more',
+		'.sc-button-addtoset',
+		'.addToPlaylistButton[title=""]',
+		'button[title="Close"]',
+	];
+
+	addToPlaylist(selectorsAll);
+
+	function addToPlaylist (_selectors) {
+		const selectors = [..._selectors];
+
+		selecting(selectors.shift())
+			.then(element => {
+				element.click();
+
+				if (selectors.length) {
+					addToPlaylist(selectors);
+				} else {
+					document.querySelector('.soundList__item').remove();
+
+					setTimeout(() => addToPlaylist([...selectorsAll]), 500);
+				}
+			})
+			.catch(err => {
+				console.error('Something went wrong!');
+				console.trace(err);
+			});
+	}
+
+	function selecting (selector) {
+		return new Promise((resolve, reject) => {
+			const pooling = setInterval(() => {
+				const element = document.querySelector(selector);
+
+				if (element) {
+					resolve(element);
+				}
+			}, 100);
+
+			setTimeout(() => {
+				clearInterval(pooling);
+				reject();
+			}, 5000);
+		});
+	}
+})();
